a8e4b918b6f1a3a5cbbb226e1934612da6d56592
Andrew <andrewriznykua93@gmail.com>
2019-09-09 12:50:25 -0500

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,13 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
-welcome
-character = get_character_from_user
-show_character_movies(character)
+
+
+# show_character_movies(character)
+def run
+    welcome
+    character = get_character_from_user
+    show_character_movies(character)
+end
+
+run
